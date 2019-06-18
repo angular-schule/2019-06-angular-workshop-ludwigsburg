95fbf73e28178bb1786b3685fea2efae71ecbd1d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2019-06-18 14:55:05 +0200

    a perfect presentational component

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -1,18 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookComponent {
 
-  @Input()
-  book: Book;
+  @Input() book: Book;
 
-  getStars() {
-    return new Array(this.book.rating);
+  @Output() rateDown = new EventEmitter<Book>();
+  @Output() rateUp = new EventEmitter<Book>();
+
+  doRateDown() {
+    this.rateDown.emit(this.book);
   }
 
+  doRateUp() {
+    this.rateUp.emit(this.book);
+  }
 }
